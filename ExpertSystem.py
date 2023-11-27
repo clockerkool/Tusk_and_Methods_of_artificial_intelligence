511d6461aeae718953a31eb8b9f46c295651f234
clockerkool <105645239+clockerkool@users.noreply.github.com>
2023-11-27 19:56:02 +0500

    Update ExpertSystem.py

--- ExpertSystem.py
+++ ExpertSystem.py
@@ -9,8 +9,8 @@ class Data:
         with sql.connect("Concept.db") as con:
             cur = con.cursor()
             cur.execute(f"SELECT name, find FROM concepts1 WHERE find != ?", ("None", ))
-            data1, data2 = self.change_type_data(cur.fetchall())
-            return data1, data2
+            data1, data2, data3 = self.change_type_data(cur.fetchall())
+            return data1, data2, data3
 
     def get_final_predicts(self, hard_concept):
         recomends = []
@@ -27,34 +27,45 @@ class Data:
         """Функция для преобразования данных из БД в формат словаря"""
         simple_consepts = {} # Здесь будут хранится факты, в которых нет условия ИЛИ
         consepts = {} # Здесь будут хранится факты, в которых есть ИЛИ
+        linked_consepts = {}
         for elem in data:
-            if "or" not in elem[1]:
+            if "or" not in elem[1] and ")" not in elem[1]:
                simple_consepts[elem[0]] = elem[1].split(", ")
-            else:
+            elif "or" in elem[1] and ")" not in elem[1]:
                 consepts[elem[0]] = [elem.split(",") for elem in elem[1].split("or")]
-
-        return simple_consepts, consepts
+            elif ")" in elem[1]:
+                linked_consepts[elem[0]] = elem[1].split(",")
+        return simple_consepts, consepts, linked_consepts
 
 
 class ExpertSystem:
     def __init__(self, concepts):
         data = Data()
         self.concepts = concepts
-        self.data1, self.data2 = data.get_initial_data()
-        self.find_concepts(self.data1, self.data2)
+        self.data1, self.data2, self.data3 = data.get_initial_data()
+        self.find_concepts(self.data1, self.data2, self.data3)
         self.recomends = data.get_final_predicts(self.hard_concept)
         self.answer = self.get_answer(self.recomends)
 
-    def find_concepts(self, data1, data2):
+    def find_concepts(self, data1, data2, data3):
         self.hard_concept = []
-        for i in range(10):
+        i = 0
+        temp = None
+        while temp != len(self.hard_concept):
+            temp = len(self.hard_concept)
             for key, value in data1.items():
                 if all(elem in self.hard_concept or elem in self.concepts for elem in value) and key not in self.hard_concept:
                     self.hard_concept.append(key)
             for key, value in data2.items():
                 for i in value:
-                    if any(elem in self.hard_concept or elem in self.concepts for elem in i):
+                    if any(elem in self.hard_concept or elem in self.concepts for elem in i) and key not in self.hard_concept:
                         self.hard_concept.append(key)
+            for key, value in data3.items():
+                temp_list = self.hard_concept + self.concepts
+                if all(any(temp in elem for temp in temp_list) for elem in value) and key not in self.hard_concept:
+                    self.hard_concept.append(key)
+
+
 
 
     def get_answer(self, recomends):
@@ -80,31 +91,51 @@ class MainWindow(QtWidgets.QMainWindow, DialogWindowES_ver2.Ui_MainWindow):
     def __get_answers(self):
         if self.answer1.isChecked():
             self.concepts.append("ХЛ")
+        if self.negative_answer1.isChecked():
+            self.concepts.append("!ХЛ")
 
         if self.answer2.isChecked():
             self.concepts.append("С")
 
+        if self.negative_answer2.isChecked():
+            self.concepts.append("!С")
+
         if self.answer3.isChecked():
             self.concepts.append("РК")
+        if self.negative_answer3.isChecked():
+            self.concepts.append("!РК")
 
         if self.answer4.isChecked():
             self.concepts.append("НЛ")
+        if self.negative_answer4.isChecked():
+            self.concepts.append("!НЛ")
 
         if self.answer5.isChecked():
             self.concepts.append("ИоЛ")
+        if self.negative_answer5.isChecked():
+            self.concepts.append("!ИоЛ")
 
         if self.answer6.isChecked():
             self.concepts.append("ОВ")
+        if self.negative_answer6.isChecked():
+            self.concepts.append("!ОВ")
 
         if self.answer7.isChecked():
             self.concepts.append("ЗП")
+        if self.negative_answer7.isChecked():
+            self.concepts.append("!ЗП")
 
         if self.answer8.isChecked():
             self.concepts.append("ОП")
 
+        if self.negative_answer8.isChecked():
+            self.concepts.append("!ОП")
+
         if self.answer9.isChecked():
             self.concepts.append("Л19В")
-        print(self.concepts)
+        if self.negative_answer9.isChecked():
+            self.concepts.append("!Л19В")
+
 
     def get_final_result(self):
         self.__get_answers()
